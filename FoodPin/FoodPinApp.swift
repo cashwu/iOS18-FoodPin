99907d59c4b1d592a1a88003a6baf205f0acffcf
cash <cashwugeek@icloud.com>
2025-12-31 21:32:38 +0800

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Cash on 2025-12-31.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
