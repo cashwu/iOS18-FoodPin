b255a79cfc8bda6faa6cf886e3348698a56ffa40
cash <cashwugeek@icloud.com>
2025-12-31 21:56:25 +0800

    7.4

---
+++ FoodPin/RestaurantListView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  FoodPin
+//
+//  Created by Cash on 2025-12-31.
+//
+
+import SwiftUI
+
+struct RestaurantListView: View {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+    
+    var body: some View {
+        List {
+            ForEach(restaurantNames.indices, id:\.self) { index in
+                
+                HStack {
+                    
+                    Image(restaurantImages[index])
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    
+                    Text(restaurantNames[index])
+                }
+                
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+#Preview {
+    RestaurantListView()
+}
