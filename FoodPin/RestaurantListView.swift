ed1cc3244fcf6ca17882715cbd3c6e373fb23836
cash <cashwugeek@icloud.com>
2025-12-31 22:30:56 +0800

    8.1

--- FoodPin/RestaurantListView.swift
+++ FoodPin/RestaurantListView.swift
@@ -17,29 +17,16 @@ struct RestaurantListView: View {
         List {
             ForEach(restaurantNames.indices, id:\.self) { index in
                 
-                HStack(alignment: .top, spacing: 20) {
-                    
-                    Image(restaurantImages[index])
-                        .resizable()
-                        .frame(width: 120, height: 118)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                    
-                    VStack(alignment: .leading) {
-                        
-                        Text(restaurantNames[index])
-                            .font(.system(.title2, design: .rounded))
-                        
-                        Text("Type")
-                            .font(.system(.body, design: .rounded))
-                        
-                        Text("Location")
-                            .font(.system(.subheadline, design: .rounded))
-                            .foregroundStyle(.gray)
-
-                    }
-                    
-                }
                 
+//                BasicTextImageRow(imageName: restaurantImages[index],
+//                                  name: restaurantNames[index],
+//                                  type: "type",
+//                                  location: "location")
+                
+                FullImageRow(imageName: restaurantImages[index],
+                                  name: restaurantNames[index],
+                                  type: "type",
+                                  location: "location")
             }
             
             .listRowSeparator(.hidden)
@@ -47,6 +34,80 @@ struct RestaurantListView: View {
         .listStyle(.plain)
     }
 }
+
+struct BasicTextImageRow : View {
+    
+    var imageName: String
+    var name:String
+    var type:String
+    var location:String
+    
+    var body: some View {
+        
+        HStack(alignment: .top, spacing: 20) {
+            
+            Image(imageName)
+                .resizable()
+                .frame(width: 120, height: 118)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+            VStack(alignment: .leading) {
+                
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundStyle(.gray)
+                
+            }
+            
+        }
+    }
+
+}
+
+struct FullImageRow : View {
+    
+    var imageName: String
+    var name:String
+    var type:String
+    var location:String
+    
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: 10) {
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+            VStack(alignment: .leading) {
+                
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundStyle(.gray)
+                
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+            
+        }
+    }
+
+}
+
 
 #Preview {
     RestaurantListView()
