f15549e2c4d76d0e047ef642dc5bfa5603921e17
cash <cashwugeek@icloud.com>
2026-01-02 21:35:25 +0800

    9.8

--- FoodPin/Restaurant.swift
+++ FoodPin/Restaurant.swift
@@ -8,25 +8,34 @@
 import Foundation
 
 struct Restaurant {
-    var name: String
-    var type: String
-    var location: String
-    var image: String
-    var isFavorite: Bool
-    
-    init(name: String, type: String, location: String, image: String, isFavorite: Bool) {
-        self.name = name
-        self.type = type
-        self.location = location
-        self.image = image
-        self.isFavorite = isFavorite
-    }
-    
-    init() {
-        self.name = ""
-        self.type = ""
-        self.location = ""
-        self.image = ""
-        self.isFavorite = false
-    }
+    var name: String = ""
+    var type: String = ""
+    var location: String = ""
+    var image: String = ""
+    var isFavorite: Bool = false
 }
+
+
+//struct Restaurant {
+//    var name: String
+//    var type: String
+//    var location: String
+//    var image: String
+//    var isFavorite: Bool
+//    
+//    init(name: String, type: String, location: String, image: String, isFavorite: Bool) {
+//        self.name = name
+//        self.type = type
+//        self.location = location
+//        self.image = image
+//        self.isFavorite = isFavorite
+//    }
+//    
+//    init() {
+//        self.name = ""
+//        self.type = ""
+//        self.location = ""
+//        self.image = ""
+//        self.isFavorite = false
+//    }
+//}
