7ba06233438b7112c167ff601bdadfbea1cb47a2
cash <cashwugeek@icloud.com>
2026-01-02 21:32:50 +0800

    9.5

---
+++ FoodPin/Restaurant.swift
@@ -0,0 +1,32 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Cash on 2026-01-02.
+//
+
+import Foundation
+
+struct Restaurant {
+    var name: String
+    var type: String
+    var location: String
+    var image: String
+    var isFavorite: Bool
+    
+    init(name: String, type: String, location: String, image: String, isFavorite: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isFavorite = isFavorite
+    }
+    
+    init() {
+        self.name = ""
+        self.type = ""
+        self.location = ""
+        self.image = ""
+        self.isFavorite = false
+    }
+}
