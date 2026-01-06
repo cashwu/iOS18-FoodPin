b4f536fdb1ede3eb736f8c07a1d3a8dc8f7bbff2
cash <cashwugeek@icloud.com>
2026-01-06 21:45:00 +0800

    11.3

--- FoodPin/View/RestaurantListView.swift
+++ FoodPin/View/RestaurantListView.swift
@@ -40,22 +40,27 @@ struct RestaurantListView: View {
             List {
                 ForEach(restaurants.indices, id:\.self) { index in
                     
-                    BasicTextImageRow(restaurant: $restaurants[index])
-                        .swipeActions(edge:.leading,allowsFullSwipe: false) {
-                            Button {
-                                
-                            } label: {
-                                Image(systemName: "heart")
-                            }
-                            .tint(.green)
-                            
-                            Button {
-                                
-                            } label: {
-                                Image(systemName: "square.and.arrow.up")
-                            }
-                            .tint(.orange)
-                        }
+                    NavigationLink(destination: RestaurantDetailView(restaurant: restaurants[index])) {
+                        BasicTextImageRow(restaurant: $restaurants[index])
+                    }
+                
+                    
+//                    BasicTextImageRow(restaurant: $restaurants[index])
+//                        .swipeActions(edge:.leading,allowsFullSwipe: false) {
+//                            Button {
+//                                
+//                            } label: {
+//                                Image(systemName: "heart")
+//                            }
+//                            .tint(.green)
+//                            
+//                            Button {
+//                                
+//                            } label: {
+//                                Image(systemName: "square.and.arrow.up")
+//                            }
+//                            .tint(.orange)
+//                        }
                 }
                 .onDelete(perform: { indexSet in
                     restaurants.remove(atOffsets:indexSet)
