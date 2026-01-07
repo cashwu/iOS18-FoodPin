420db7a9f6f30b5afd28d968991f48a73f78ac14
cash <cashwugeek@icloud.com>
2026-01-07 23:04:39 +0800

    13.3

--- FoodPin/FoodPinApp.swift
+++ FoodPin/FoodPinApp.swift
@@ -14,8 +14,8 @@ struct FoodPinApp: App {
         
         let navBarAppearance = UINavigationBarAppearance()
         
-        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(red: 218, green: 96, blue: 51), .font: UIFont(name: "ArialRoundedMTBold", size: 35)!]
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor(red: 218, green: 96, blue: 51), .font: UIFont(name: "ArialRoundedMTBold", size: 20)!]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(named: "NavigationBarTitle") ?? UIColor.gray, .font: UIFont(name: "ArialRoundedMTBold", size: 35)!]
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor(named: "NavigationBarTitle") ?? UIColor.gray, .font: UIFont(name: "ArialRoundedMTBold", size: 20)!]
 
         navBarAppearance.backgroundColor = .clear
         navBarAppearance.backgroundEffect = .none
